e8ab747ccccfbf0fb8bbc4e9d174c73e702a9dff
developer-cmd-dev <devkmandal0@gmail.com>
2025-03-04 21:43:29 +0530

    Project Initialized

---
+++ src/main/java/com/urlshortner/UrlShortner/UrlShortnerServerApplication.java
@@ -0,0 +1,13 @@
+package com.urlshortner.UrlShortner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortnerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShortnerServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/urlshortner/UrlShortner/UrlShortnerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urlshortner.UrlShortner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortnerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
